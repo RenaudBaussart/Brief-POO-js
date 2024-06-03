75301d00c9b651d1988aca5b493377c6548c2c17
renaudbaussart <renaud.baussart@proton.me>
2024-06-03 16:39:31 +0200

    feat(01.js): done task O1

---
+++ src/01-classes-and-objects.js
@@ -0,0 +1,14 @@
+class Voiture{
+    constructor(marque,modele,annee,couleur){
+        this.marque = marque
+        this.modele = modele;
+        this.annee = annee;
+        this.couleur = couleur;
+    }
+    afficherDetails(){
+    console.log('une voiture de marque ' + this.marque + ' de modele ' + this.modele + ' de ' + this.annee + ' couleur ' + this.couleur);
+    }
+}   
+
+let voitureOne = new Voiture('mercedes','Benz Classe C','2010','gris')
+voitureOne.afficherDetails();
